7cba3b9243edc1c781bc078cb1e2942be2a45b38
Greg Hurlock <greg@shinygrey.com>
2018-10-27 10:07:15 +0100

    for deploy

---
+++ server.js
@@ -0,0 +1,10 @@
+const express= require('express'),
+path = require('path');
+const app =express();
+app.use(express.static('./dist/shinyangle1'));
+app.get('/*', (req,res)=>{
+	res.sendFile(path.join(__dirname,'/dist/shinyangle1/index.html'));
+});
+app.listen(process.env.PORT || 8080, ()=>{
+	console.log('Server started');
+})
