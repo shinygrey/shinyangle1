62e989cca934d4b53b24b58ab5f7d92726810113
Greg Hurlock <greg@shinygrey.com>
2018-11-04 20:18:06 +0000

    backend added

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
-const express= require('express'),
-path = require('path');
+const http = require("http");
+const https = require("https");
+const path = require('path');
+const fs = require("fs");
+
+const express= require('express');
 const app =express();
 
 app.use(express.static('./dist/shinyangle1'));
 
-app.get('/*', (req,res)=>{
-	res.sendFile(path.join(__dirname,'/dist/shinyangle1/index.html'));
+app.use('/backend',function(req, res, next){
+
+	var theresponse = process.env.GREG_VAR
+	var allowedFromConfig = process.env.ALLOWED_SITES;
+	var allowedOrigins = allowedFromConfig.split(",");
+	
+	if((req.get('Referer') == ("https://shinyangle-staging.herokuapp.com") && req.get('Origin') == undefined)){
+		res.send("\n"+theresponse+"\n");
+		next();	
+	}else if(allowedOrigins.includes(req.get('Origin'))){
+		res.header("Access-Control-Allow-Origin", req.get('Origin'));
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		res.send("\n"+theresponse+"\n");
+		next();
+	}else{
+		res.send('not accessible');
+		res.status(403).end('forbidden');
+	}
 });
 
-app.get('/backend', function(req, res){
-	res.send("ohai")  //process.env.GREG_VAR
+app.get('/*', (req,res)=>{
+	res.sendFile(path.join(__dirname,'/dist/shinyangle1/index.html'));
 });
 
 app.listen(process.env.PORT || 8080, ()=>{
