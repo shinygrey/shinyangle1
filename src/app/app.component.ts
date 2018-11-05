4068da0b40b39d41f09518400055752a91db3aa9
Greg Hurlock <greg@shinygrey.com>
2018-11-05 18:54:09 +0000

    Rest test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,13 @@ export class AppComponent {
 	constructor(private requests: RequestService){}
 	jsonDataFromBackend;
 	ngOnInit(){
-		this.requests.getBackEndVariable().subscribe((data: string) => console.log("app.component.ts says: GET successful\n"));
-		
-		try{
-			this.jsonDataFromBackend = JSON.parse(data);
-		}catch(error){console.error(error);}
-		
+		this.requests.getBackEndVariable().subscribe(
+			(data: string) => console.log("app.component.ts says: GET successful\n")
+			try{
+				this.jsonDataFromBackend = JSON.parse(data);
+			}catch(error){console.error(error);}
 		);
+
 		try{
 			console.log(this.jsonDataFromBackend.total);
 		}catch(error){console.error(error);}
