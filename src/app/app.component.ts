d6904d1a988053a7ea08c824ff4a33f887458b3b
Greg Hurlock <greg@shinygrey.com>
2018-11-05 19:19:40 +0000

    Rest test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,17 +12,10 @@ export class AppComponent {
 		this.requests.getBackEndVariable().subscribe((data: string) => {
 			console.log("app.component.ts says: GET successful\n");
 			try{
-				this.jsonDataFromBackend = JSON.parse(data);
+				var jsonData = JSON.parse(data);
+				console.error(jsonData.Results[0].Id);
 			}catch(error){console.error(error);}
 		});
- 
-		
-		
-		
-
-		try{
-			console.log(this.jsonDataFromBackend.total);
-		}catch(error){console.error(error);}
 	}
 	
 	title = 'The Shiny Grey Angular Portfolio';
